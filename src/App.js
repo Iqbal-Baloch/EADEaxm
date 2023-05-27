ea0e48e313801a0a4e75e57034a9a51cc3d3dd08
Iqbal-Baloch <iqbalsyedmuhammad1@gmail.com>
2023-05-27 16:32:03 +0500

    -no-edit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { useState } from 'react';
 import './App.css';
 import PollDisplay from './PollDisplay.js';
 function App() {
+  const simpleData = {
+    "question": "What is your favorite programming language?",
+    "choices": [
+    { "id": 1, "label": "JavaScript", "votes": 0 },
+    { "id": 2, "label": "Python", "votes": 0 },
+    { "id": 3, "label": "Java", "votes": 0 },
+    { "id": 4, "label": "C#", "votes": 0 }
+  ]
+  }
+  const [data, setData] = useState(simpleData);
   return (
     <div className="App">
       <h1 align="center">Polling App</h1>
-      <PollDisplay javascriptVote={1} pythonVote={3} javaVote={4} cVote={4}/>
+      <PollDisplay data={data}/>
       
     </div>
   );
--- src/PollDisplay.js
+++ src/PollDisplay.js
@@ -1,15 +1,16 @@
 export default function PollDisplay(props){
-    const {javascriptVote, pythonVote, javaVote, cVote} = props ;
+    
     return(
         <div style={{margin: "2rem"}}>
-            <h2>Poll Question: What is your favorite programming language?</h2>
+            <h2>Poll Question: {props.data.question}</h2>
           
             <h3>Answer Choices with Vote Counts:</h3>
             <ul>
-                <li>JavaScript: {javascriptVote} votes</li>
-                <li>Python: {pythonVote} votes</li>
-                <li>Java: {javaVote} votes</li>
-                <li>C#: {cVote} votes</li>
+                {props.data.choices.map(options =>{
+                    return(
+                        <li key={options.id}>{options.label}: {options.votes}</li>
+                    );
+                })}
             </ul>
         </div>
     );
